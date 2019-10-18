f9d69ead53677d31dc7b807a8fbaad438c5fbb8a
Konrad Krawczyk <logan15.krawczyk@gmail.com>
2019-10-18 15:29:01 +0200

    fixed validation edit token

--- src/pl/femax/controllers/BorderEditController.java
+++ src/pl/femax/controllers/BorderEditController.java
@@ -28,9 +28,26 @@ public class BorderEditController {
     private TextField producentTextField;
     @FXML
     private TextField tokenTextField;
+    private String str[] = new String[10];
 
     @FXML
-    public void initialize() {
+    public void initialize() throws IOException {
+        File f = new File("token.txt");
+        BufferedReader r;
+        int i= 0;
+        try {
+            r = new BufferedReader(new FileReader(f));
+            String readLine ="";
+            while((readLine = r.readLine()) != null){
+                str[i] = readLine;
+                i++;
+            }
+            logText.setText("Tokeny załadowanie poprawnie");
+
+        } catch (FileNotFoundException e) {
+            logText.setText("Błąd podczas ładowania tokenów");
+            logText.setStyle("-fx-fill: red; -fx-font-size: 25px;");
+        }
         producentTableColumn.setCellValueFactory(new PropertyValueFactory<>("producent"));
         tokenTableColumn.setCellValueFactory(new PropertyValueFactory<>("token"));
         tokenTableView.setItems(getProducent());
@@ -41,34 +58,35 @@ public class BorderEditController {
     public void updateToken() {
         producentTextField.setText(producentTextField.getText().replaceAll(" ", ""));
         tokenTextField.setText(tokenTextField.getText().replaceAll(" ", ""));
-        if (producentTextField.getLength() == 0 || tokenTextField.getLength() == 0) {
-            if (producentTextField.getLength() == 0 && tokenTextField.getLength() == 0)
+        if (producentTextField.getText().isEmpty()|| tokenTextField.getText().isEmpty()) {
+            if (producentTextField.getText().isEmpty() && tokenTextField.getText().isEmpty()) {
                 logText.setText("Podaj Producenta i Token");
-            else if (tokenTextField.getLength() == 0)
-                logText.setText("Podaj Token");
-            else
-                logText.setText("Podaj Producenta");
-            logText.setStyle("-fx-fill: red; -fx-font-size: 25px;");
-        }
-        String str = producentTextField.getText();
-        System.out.println("str = " + str);
-        String str1 = products.get(0).getProducent();
-        System.out.println("str1 = " + str1);
-        System.out.println(str.compareTo(str1));
-        products.forEach(x -> {
-            if (producentTextField.getText().equals(x.getProducent())) {
-                x.setToken(tokenTextField.getText());
-                logText.setText("Token zaktualizowany poprawnie");
-                logText.setStyle("-fx-fill: green; -fx-font-size: 25px;");
-                tokenTableView.refresh();
-            } else if (producentTextField.getText().equals(x.getProducent())) {
-                logText.setText("Niepoprawny producent");
                 logText.setStyle("-fx-fill: red; -fx-font-size: 25px;");
             }
-        });
-        System.out.println("wykonalem sie");
-        System.out.println(tokenTableColumn.getCellData(0));
-        System.out.println(products.toString());
+            else if (tokenTextField.getText().isEmpty()) {
+                logText.setText("Podaj Token");
+                logText.setStyle("-fx-fill: red; -fx-font-size: 25px;");
+            }
+            else if(producentTextField.getText().isEmpty()) {
+                logText.setText("Podaj Producenta");
+                logText.setStyle("-fx-fill: red; -fx-font-size: 25px;");
+            }
+        }
+        else if(producentTextField.getLength() != 0 && tokenTextField.getLength() != 0) {
+            for(Producent x : products){
+                if (producentTextField.getText().equals(x.getProducent())) {
+                    x.setToken(tokenTextField.getText());
+                    logText.setText("Token zaktualizowany poprawnie");
+                    logText.setStyle("-fx-fill: green; -fx-font-size: 25px;");
+                    tokenTableView.refresh();
+                    break;
+                } else if (producentTextField.getText() != x.getProducent()) {
+                    System.out.println("tu tez jestem");
+                    logText.setText("Niepoprawny producent");
+                    logText.setStyle("-fx-fill: red; -fx-font-size: 25px;");
+                }
+            }
+        }
     }
 
     public ObservableList<Producent> getProducent() {
